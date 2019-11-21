50096428b784c9b1458326da265095f48be1ba87
Stalyon <pierre.stainton@gmail.com>
2019-11-21 21:20:44 +0100

    Update PlayerRepository.java

--- src/main/java/com/stalyon/poker/repository/PlayerRepository.java
+++ src/main/java/com/stalyon/poker/repository/PlayerRepository.java
@@ -1,8 +1,10 @@
 package com.stalyon.poker.repository;
+
 import com.stalyon.poker.domain.Player;
 import org.springframework.data.jpa.repository.*;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 
 /**
  * Spring Data  repository for the Player entity.
@@ -11,4 +13,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface PlayerRepository extends JpaRepository<Player, Long> {
 
+    Optional<Player> findByName(String name);
 }
