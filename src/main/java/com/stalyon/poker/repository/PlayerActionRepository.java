7dee6a6c8c934ade9cb78cf209fbe79ee4a51ba5
Stalyon <pierre.stainton@gmail.com>
2019-11-21 23:33:38 +0100

    Update PlayerActionRepository.java

--- src/main/java/com/stalyon/poker/repository/PlayerActionRepository.java
+++ src/main/java/com/stalyon/poker/repository/PlayerActionRepository.java
@@ -1,8 +1,11 @@
 package com.stalyon.poker.repository;
+
+import com.stalyon.poker.domain.Hand;
 import com.stalyon.poker.domain.PlayerAction;
 import org.springframework.data.jpa.repository.*;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 
 /**
  * Spring Data  repository for the PlayerAction entity.
@@ -11,4 +14,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface PlayerActionRepository extends JpaRepository<PlayerAction, Long> {
 
+    List<PlayerAction> findAllByHand(Hand hand);
 }
